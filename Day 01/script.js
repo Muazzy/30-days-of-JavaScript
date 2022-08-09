e26fa28a21c8317072ff3b76926a8e9caa617652
muazzam <muazzamafaque@gmail.com>
2022-08-10 00:09:09 +0500

    got a refresher, done with Day 01

---
+++ Day 01/script.js
@@ -0,0 +1,60 @@
+// Day 1: Exercises
+
+//1: comments can make code readable
+// 2: 30DaysOfJavaScript
+
+/*
+3: comments can make code readable, easy to reuse and informative
+*/
+
+// Exercise 4
+// let st = "String";
+// let bol = false;
+// // let undefined; //you cannot do this.
+// //but you can do this,
+// var undefined;
+// //so this null will be object typeof
+// let lul = null;
+
+
+// // Exercise 5 
+// // alert(typeof st);
+// // alert(typeof bol);
+// alert(typeof undefined);
+// alert(typeof lul);
+
+
+// 6. Declare four variables without assigning values
+// var v1;
+// var v2;
+// var v3;
+// var v4;
+
+// // 7. Declare four variables with assigned values
+// let x1 = "x1";
+// let num1 = 1;
+// let muz = 114;
+// let off = false;
+
+
+
+// 8. Declare variables to store your first name, last name, marital status, country and age in multiple lines
+
+// let fName = "muazzy";
+// let lName = "soomro";
+// let mStatus = ".";
+// let country = "Pakistan";
+// let age = 21;
+
+//9. Declare variables to store your first name, last name, marital status, country and age in multiple lines
+
+//this let will apply to all of em
+// let fName = "muazzy", lName = "soomro", mStatus = ".", country = "Pakistan", age = 21;
+
+// 10. Declare two variables myAge and yourAge and assign them initial values and log to the browser console.
+
+let myAge = 21;
+let yourAge = 18;
+
+// console.log(myAge + " " + yourAge);
+alert(myAge + " " + yourAge);
