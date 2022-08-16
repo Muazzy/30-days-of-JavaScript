7c057e1e8ebc9ec26157eb59a2573eddeac2e30a
muazzam <muazzamafaque@gmail.com>
2022-08-16 23:52:15 +0500

    Day 03 Completed

--- Day 03/script.js
+++ Day 03/script.js
@@ -1,1 +1,14 @@
 console.log("im in babe");
+
+const TodaysDate = new Date();
+console.log(TodaysDate);
+console.log(TodaysDate.getFullYear()); //2022
+console.log(TodaysDate.getDate()); //16
+console.log(TodaysDate.getDay()); //Tuesday (2) #day of the week
+console.log(TodaysDate.getHours()); //23 (11pm)
+console.log(TodaysDate.getMinutes()); //38 (11:38)
+console.log(TodaysDate.getSeconds()); // 35 (11:38:35)
+
+var loru = parseInt(9.8);
+
+console.log(loru === 10);
