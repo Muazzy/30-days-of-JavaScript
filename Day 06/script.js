cd737a50e6cc1b4745c0af12bb23e25ad8693e32
muazzam <muazzamafaque@gmail.com>
2022-08-20 15:47:26 +0500

    almost done

--- Day 06/script.js
+++ Day 06/script.js
@@ -57,7 +57,7 @@
 //     }
 //     console.log(i)
 //   }
-  
+
 //   // 0 1 2
 // The above code stops if 3 found in the iteration process.
 
@@ -70,8 +70,147 @@
 //     }
 //     console.log(i)
 //   }
-  
+
 //   // 0 1 2 4 5
 
 
+const countries = [
+    'Albania',
+    'Bolivia',
+    'Canada',
+    'Denmark',
+    'Ethiopia',
+    'Finland',
+    'Germany',
+    'Hungary',
+    'Ireland',
+    'Japan',
+    'Kenya'
+]
 
+const webTechs = [
+    'HTML',
+    'CSS',
+    'JavaScript',
+    'React',
+    'Redux',
+    'Node',
+    'MongoDB'
+]
+
+const mernStack = ['MongoDB', 'Express', 'React', 'Node']
+
+// Iterate 0 to 10 using for loop, do the same using while and do while loop
+
+// Iterate 10 to 0 using for loop, do the same using while and do while loop
+
+// Iterate 0 to n using for loop
+
+
+for (let i = 0; i <= 10; i++) {
+    console.log('for loop ' + i);
+}
+
+let i = 0;
+while (i <= 10) {
+    console.log('while loop ' + i);
+    i++;
+}
+
+let x = 0;
+do {
+    console.log('do while loop ' + x)
+    x++
+} while (x <= 10);
+
+
+// You can use only expressions inside of a conditional (ternary) operator ?:. A for statement is not an expression. //THAT IS WHY THIS IS NOT WORKING
+function itterateTillN(n, isIncrement) {
+    if (n === 0) {
+        console.log('loop aint applicable')
+    }
+    else if (isIncrement) {
+        for (let i = 0; i <= n; i++) {
+            console.log(i)
+        }
+    }
+    else {
+        for (let j = n; j >= 0; j--) {
+            console.log(j)
+        }
+    }
+}
+
+itterateTillN(1, false);
+
+let hash = '#';
+
+for (let i = 1; i <= 7; i++) {
+    console.log(hash.repeat(i));
+}
+
+for (let a = 0; a <= 10; a++) {
+    console.log(`${a} x ${a} = ${a * a}`)
+}
+
+for (let a = 0; a <= 10; a++) {
+    console.log(`${a}  ${a ** 2}  ${a ** 3}`)
+}
+
+//printing even numbers
+// for (let e = 0; e <= 100; e++) {
+//     if (e % 2 === 0) {
+//         console.log(e);
+//     }
+// }
+
+// //printing odd numbers
+// for (let e = 0; e <= 100; e++) {
+//     if (e % 2 !== 0) {
+//         console.log(e);
+//     }
+// }
+
+// for (let p = 2; p <= 100; p++) {
+
+//     let Isdivisible = false; //to check if the number 'p' is divisible by anyother number other than it self
+
+//     //looping from 2 thru the number p-1 (one less number than p)
+//     for (let i = 2; i < p; i++) {
+//         //checking if the p is totally divisible by another number other than itself
+//         if (p % i === 0) {
+//             //if it is divisble then setting it to true
+//             Isdivisible = true;
+//             //breaking the loop cuz we now know it wont be a prime number cuz it got divided by another number that was not itself
+//             break;
+//         }
+//     }
+//     //if the number did not get divide by another number then printing it out cuz its  a prime number.
+//     if (!Isdivisible) {
+//         console.log(p);
+//     }
+// }
+
+// sum of all numbers 0 - 100
+
+// let sum = 0;
+// for (let i = 0; i <= 100; i++) {
+//     sum += i;
+// }
+// console.log(sum)
+
+// let evenSum = 0;
+// let oddSum = 0;
+
+// for (i = 0; i <= 100; i++) {
+//     i % 2 === 0 ? evenSum += i : oddSum += i
+// }
+
+// console.log(`The sum of all evens from 0 to 100 is ${evenSum}`)
+// console.log(`The sum of all odds from 0 to 100 is ${oddSum}`)
+
+// console.log([evenSum, oddSum])
+
+let random = Math.floor(Math.random());
+// random = Math.floor(random);
+// console.log(random * 10)
