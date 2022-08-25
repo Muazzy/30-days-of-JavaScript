b23ccb32afe70af5a6c3d460ba0e2e8f2922463e
muazzam <muazzamafaque@gmail.com>
2022-08-25 12:49:54 +0500

    start of the day

--- Day 09/script.js
+++ Day 09/script.js
@@ -1,1 +1,15 @@
 //Continue from Callback function
+
+// A callback function is a function which can be passed in as a parameter to other function
+
+// example:
+//you can name the function anything you want
+const callbackFunction = (n) => n ** 2; //callback function 
+
+//function that takes other function as a callback
+
+function cube(callback, n) {
+    return callback(n) * n
+}
+
+console.log(cube(callbackFunction, 3))
