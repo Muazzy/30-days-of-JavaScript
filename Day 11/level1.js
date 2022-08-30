0350afc2dffd115bee1ffae8796b275f20d61a61
muazzam <muazzamafaque@gmail.com>
2022-08-30 23:38:10 +0500

    lvl2 complete

--- Day 11/level1.js
+++ Day 11/level1.js
@@ -1,49 +1,3 @@
-
-const users = [
-    {
-        name: 'Brook',
-        scores: 75,
-        skills: ['HTM', 'CSS', 'JS'],
-        age: 16
-    },
-    {
-        name: 'Alex',
-        scores: 80,
-        skills: ['HTM', 'CSS', 'JS'],
-        age: 18
-    },
-    {
-        name: 'David',
-        scores: 75,
-        skills: ['HTM', 'CSS'],
-        age: 22
-    },
-    {
-        name: 'John',
-        scores: 85,
-        skills: ['HTML'],
-        age: 25
-    },
-    {
-        name: 'Sara',
-        scores: 95,
-        skills: ['HTM', 'CSS', 'JS'],
-        age: 26
-    },
-    {
-        name: 'Martha',
-        scores: 80,
-        skills: ['HTM', 'CSS', 'JS'],
-        age: 18
-    },
-    {
-        name: 'Thomas',
-        scores: 90,
-        skills: ['HTM', 'CSS', 'JS'],
-        age: 20
-    }
-]
-
 // Destructure and assign the elements of constants array to e, pi, gravity, humanBodyTemp, waterBoilingTemp.
 const constants = [2.72, 3.14, 9.81, 37, 100]
 const [e, pi, gravity, humanBodyTemp, waterBoilingTemp] = constants
@@ -65,3 +19,4 @@ const rectangle = {
 let { width, height, area, perimeter } = rectangle
 
 console.log(width, height, area, perimeter)
+
