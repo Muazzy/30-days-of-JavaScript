4c328827c11bff005c1d12a23f2fc80bbdcfb8bc
muazzam <muazzamafaque@gmail.com>
2022-08-31 22:17:36 +0500

    start of RegExp

---
+++ Day 12/script.js
@@ -0,0 +1,8 @@
+// Parameters of the regex
+// let regExpPattern = new RegExp(pattern, flag) //pattern:String, flag: a flag or a combination of flags
+
+// for example: 
+let pattern = 'hate'
+let flag = 'gi' //g: global & i: case insensitive
+
+
