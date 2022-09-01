d21cde2c8b1820794cf3d5bf4d7454b161722888
muazzam <muazzamafaque@gmail.com>
2022-09-02 00:40:19 +0500

    done with RegExp

--- Day 12/script.js
+++ Day 12/script.js
@@ -102,3 +102,56 @@ console.log(matches) //I am teacher and  I love teaching.There is nothing as mor
 
 
 //continue from Set of characters cheatsheat
+
+
+
+// to add some set of characters: [starting range - end range]
+// [a-c] means, a or b or c
+// [a-z] means, any letter a to z
+// [A-Z] means, any character A to Z
+// [0-3] means, 0 or 1 or 2 or 3
+// [0-9] means any number 0 to 9
+// [A-Za-z0-9] any character which is a to z, A to Z, 0 to 9
+
+
+// \: uses to escape special characters
+
+// to match where the sring contains digits (numbers from 0 - 9)
+// to match where the string does NOT contain digits
+
+// . (any character except new line character (\n)  )  
+
+// ^ (starts with) can be used as r'^full' , a sentence which starts with the word full
+// r'[^abc] mean not a, not b, not c.
+
+
+// $ (ends with),  r'substring$' eg r'love$', sentence ends with a word love
+
+
+// *: zero or more times
+// r'[a]*' means a optional or it can occur many times.
+
+// +: one or more times
+// r'[a]+' means at least once or more times
+
+// ?: zero or one times
+// r'[a]?' means zero times or once
+
+// \b: word bounder, matches with the beginning or ending of a word
+
+
+// {3}: Exactly 3 characters
+
+// {3,}: At least 3 characters
+
+// {3,8}: 3 to 8 characters
+
+// |: Either or
+
+// r'apple|banana' mean either of an apple or a banana
+
+// (): Capture and group
+
+
+// To learn more and to create different RegExp patterns for different use cases:
+//  https://regex101.com/
