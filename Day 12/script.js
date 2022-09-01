ac44d28cfe894928de375ad3497d842ae6ece148
muazzam <muazzamafaque@gmail.com>
2022-09-01 13:03:40 +0500

    second commit

--- Day 12/script.js
+++ Day 12/script.js
@@ -6,3 +6,30 @@ let pattern = 'hate'
 let flag = 'gi' //g: global & i: case insensitive
 
 
+// Declaring a regex pattern using RegExp object. Writing the pattern and the flag inside the RegExp constructor
+let regex = new RegExp('anyword', 'gim') //global, case insensitive, multiline
+
+
+// creating a RegExp without its constructor
+//         pattern, flag
+let regexWO = /full/gi;
+
+
+// here are some of the RegExp methods
+
+// 1. to test a pattern, i.e testing for a match
+// test(), Tests for a match in a string, it returns a boolean i.e true or false
+
+// example: 
+
+const str = 'i am full of cash'
+const str2 = 'i am fUlL of cash'
+const pattern1 = /full/g;
+
+//       regex, string that you want to test
+console.log(pattern1.test(str)); //true
+console.log(pattern1.test(str2)); //false, cuz the we did'nt add a case insensitive flag
+
+
+//Array containing all of the match
+// 2. match(): Returns an array containing all of the matches, including capturing groups, or null if no match is found. If we do not use a global flag, match() returns an array containing the pattern, index, input and group.
