4af56fc9786d096d6350950b55662d8f9632e7c2
muazzam <muazzamafaque@gmail.com>
2022-09-06 21:14:46 +0500

    there there

---
+++ Day 15/script.js
@@ -0,0 +1,25 @@
+// JavaScript is an object oriented programming language.Everything in JavScript is an object, with its properties and methods.We create class to create an object.
+
+// A Class is like an object constructor, or a "blueprint" for creating objects.
+
+// We instantiate a class to create an object.The class defines attributes and the behavior of the object, while the object, on the other hand, represents the class.
+
+// Once we create a class we can create object from it whenever we want. Creating an object from a class is called class instantiation.
+
+// classes helps to reduce amount of code and repetition of code.
+
+// Defining a class in js
+// To define a class 
+//use keyword "class"
+//add the name of class (it should be CamelCase)
+//with two curly braces
+
+// syntax 
+class ClassName {
+    //code here 
+}
+
+// Class instantiation
+// instantiation of a class means creating an object from a class
+const newObj = new ClassName()
+console.log(newObj) //Object {  }
